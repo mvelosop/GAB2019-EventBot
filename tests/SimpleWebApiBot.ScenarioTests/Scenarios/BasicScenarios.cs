d884a571ac394d9bd6d163cf572a6e6375712b66
Miguel Veloso <mvelosop@gmail.com>
2019-04-24 22:07:09 +0100

    Add initial testing infrastructure

---
+++ tests/SimpleWebApiBot.ScenarioTests/Scenarios/BasicScenarios.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleWebApiBot.ScenarioTests.Scenarios
+{
+    public class BasicScenarios
+    {
+        public async Task BotShouldEchoBack()
+        {
+            // Arrange -----------------
+
+            // Act ---------------------
+
+            // Assert ------------------
+
+        }
+    }
+}
