dc023c3a8df6b244038ad5291e3cd6985d906d35
Miguel Veloso <mvelosop@gmail.com>
2019-04-25 17:32:44 +0100

    Log additional events

--- src/SimpleWebApiBot/Startup.cs
+++ src/SimpleWebApiBot/Startup.cs
@@ -27,12 +27,13 @@ namespace SimpleWebApiBot
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseMiddleware<SerilogMiddleware>();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseMiddleware<SerilogMiddleware>();
             app.UseMvc();
         }
 
