017e8f70c365ff96366d0218a428a3923d1594cc
Miguel Veloso <mvelosop@gmail.com>
2019-04-25 16:37:01 +0100

    Add testing infrastructure

--- tests/SimpleWebApiBot.ScenarioTests/Scenarios/BasicScenarios.cs
+++ tests/SimpleWebApiBot.ScenarioTests/Scenarios/BasicScenarios.cs
@@ -1,12 +1,28 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using SimpleWebApiBot.ScenarioTests.Setup;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace SimpleWebApiBot.ScenarioTests.Scenarios
 {
-    public class BasicScenarios
+    public class BasicScenarios : IClassFixture<TestingHost>
     {
+        private readonly IServiceScope _scope;
+        private readonly ILogger<BasicScenarios> _logger;
+
+        public BasicScenarios(TestingHost host)
+        {
+            _scope = (host ?? throw new ArgumentNullException(nameof(host))).CreateScope();
+            _logger = GetService<ILogger<BasicScenarios>>();
+
+            _logger.LogTrace("----- INSTANCE CREATED - {ClassName}", GetType().Name);
+        }
+
+        [Fact]
         public async Task BotShouldEchoBack()
         {
             // Arrange -----------------
@@ -16,5 +32,7 @@ namespace SimpleWebApiBot.ScenarioTests.Scenarios
             // Assert ------------------
 
         }
+
+        private T GetService<T>() => _scope.ServiceProvider.GetRequiredService<T>();
     }
 }
